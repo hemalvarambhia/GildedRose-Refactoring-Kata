f7cbffba2599eeb219e746ef0bdc3ba017331ef2
hemalvarambhia <h.n.varambhia@gmail.com>
2023-09-17 23:23:52 +0100

    Added a test to confirm aged_brie before expiry date increases in quality by 1.

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -5,11 +5,13 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    def test_aged_brie_before_expiry_date(self):
+        items = [Item("Aged Brie", 5, 10)]
         gilded_rose = GildedRose(items)
+
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+
+        self.assertEqual(11, items[0].quality)
 
         
 if __name__ == '__main__':

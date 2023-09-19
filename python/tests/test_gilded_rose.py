0c033cf24dd289b0027dccaf0c2dfc862e010d20
hemalvarambhia <h.n.varambhia@gmail.com>
2023-09-19 21:08:24 +0100

    Added a test from the list - Backstage passes more than 10 days before a conference.

--- python/tests/test_gilded_rose.py
+++ python/tests/test_gilded_rose.py
@@ -41,9 +41,14 @@ class AgedBrieTest(unittest.TestCase):
 
 
 class BackstagePassesTest(unittest.TestCase):
-    @unittest.skip(reason='Test list')
+
     def test_backstage_passes_increase_in_quality_by_one_more_than_10_days_before_concert(self):
-        pass
+        backstage_passes = Item("Backstage passes to a TAFKAL80ETC concert", sell_in=11, quality=33)
+        gilded_rose = GildedRose([backstage_passes])
+
+        gilded_rose.update_quality()
+
+        self.assertEqual(34, backstage_passes.quality)
 
     @unittest.skip(reason='Test list')
     def test_backstage_passes_increase_in_quality_by_two_exactly_10_days_before_concert(self):

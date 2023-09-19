63dd494fbfb677a5cda9d1ca2af6443c90a09618
hemalvarambhia <h.n.varambhia@gmail.com>
2023-09-19 21:24:56 +0100

    Got another test passing.

--- python/tests/test_gilded_rose.py
+++ python/tests/test_gilded_rose.py
@@ -50,9 +50,13 @@ class BackstagePassesTest(unittest.TestCase):
 
         self.assertEqual(34, backstage_passes.quality)
 
-    @unittest.skip(reason='Test list')
     def test_backstage_passes_increase_in_quality_by_two_exactly_10_days_before_concert(self):
-        pass
+        backstage_passes = Item("Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=21)
+        gilded_rose = GildedRose([backstage_passes])
+
+        gilded_rose.update_quality()
+
+        self.assertEqual(23, backstage_passes.quality)
 
     @unittest.skip(reason='Test list')
     def test_backstage_passes_increase_in_quality_by_two_less_than_10_days_but_more_than_5_days_before_concert(self):

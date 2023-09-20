0d911f427bde9748ff68708aa050db0ab707514b
hemalvarambhia <h.n.varambhia@gmail.com>
2023-09-20 21:28:49 +0100

    Added another test from the list.

--- python/tests/test_gilded_rose.py
+++ python/tests/test_gilded_rose.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-
 from src.gilded_rose import Item, GildedRose
 
 
@@ -22,9 +21,13 @@ class BackstagePassesTest(unittest.TestCase):
 
         self.assertEqual(23, backstage_passes.quality)
 
-    @unittest.skip(reason="Test list")
     def test_backstage_passes_increase_in_quality_by_two_less_than_10_days_but_more_than_5_days_before_concert(self):
-        pass
+        backstage_passes = Item("Backstage passes to a TAFKAL80ETC concert", sell_in=6, quality=21)
+        gilded_rose = GildedRose([backstage_passes])
+
+        gilded_rose.update_quality()
+
+        self.assertEqual(23, backstage_passes.quality)
 
 
 if __name__ == "__main__":

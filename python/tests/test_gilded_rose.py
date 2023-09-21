4096b9c7067d8adf80b61b4e9e0a6f4ee34b4caf
hemalvarambhia <h.n.varambhia@gmail.com>
2023-09-21 22:13:36 +0100

    Added a missing test.

--- python/tests/test_gilded_rose.py
+++ python/tests/test_gilded_rose.py
@@ -29,6 +29,14 @@ class BackstagePassesTest(unittest.TestCase):
 
         self.assertEqual(23, backstage_passes.quality)
 
+    def test_backstage_passes_increase_in_quality_by_three_5_days_before_the_concern(self):
+        backstage_passes = Item("Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=12)
+        gilded_rose = GildedRose([backstage_passes])
+
+        gilded_rose.update_quality()
+
+        self.assertEqual(15, backstage_passes.quality)
+
     @unittest.skip(reason='Test list')
     def test_backstage_passes_increase_in_quality_by_three_less_than_5_days_but_more_than_1_day_before_concert(self):
         pass

b1288aaa76317bae8ae9af62228dc5b49f2a51eb
hemalvarambhia <h.n.varambhia@gmail.com>
2023-09-23 22:19:49 +0100

    Added to our test list for normal items and removed some duplicate tests that exist elsewhere.

--- python/tests/test_gilded_rose.py
+++ python/tests/test_gilded_rose.py
@@ -4,72 +4,15 @@ import unittest
 from src.gilded_rose import Item, GildedRose
 
 
-class BackstagePassesTest(unittest.TestCase):
-    def test_backstage_passes_increase_in_quality_by_one_more_than_10_days_before_concert(self):
-        backstage_passes = Item("Backstage passes to a TAFKAL80ETC concert", sell_in=11, quality=33)
-        gilded_rose = GildedRose([backstage_passes])
+class NormalItemsTest(unittest.TestCase):
 
-        gilded_rose.update_quality()
+    @unittest.skip(reason='Test list')
+    def test_normal_item_quality_that_has_not_reached_its_sell_by_date_decreases_by_1(self):
+        pass
 
-        self.assertEqual(34, backstage_passes.quality)
-
-    def test_backstage_passes_increase_in_quality_by_two_exactly_10_days_before_concert(self):
-        backstage_passes = Item("Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=21)
-        gilded_rose = GildedRose([backstage_passes])
-
-        gilded_rose.update_quality()
-
-        self.assertEqual(23, backstage_passes.quality)
-
-    def test_backstage_passes_increase_in_quality_by_two_less_than_10_days_but_more_than_5_days_before_concert(self):
-        backstage_passes = Item("Backstage passes to a TAFKAL80ETC concert", sell_in=6, quality=21)
-        gilded_rose = GildedRose([backstage_passes])
-
-        gilded_rose.update_quality()
-
-        self.assertEqual(23, backstage_passes.quality)
-
-    def test_backstage_passes_increase_in_quality_by_three_5_days_before_the_concern(self):
-        backstage_passes = Item("Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=12)
-        gilded_rose = GildedRose([backstage_passes])
-
-        gilded_rose.update_quality()
-
-        self.assertEqual(15, backstage_passes.quality)
-
-    def test_backstage_passes_increase_in_quality_by_three_less_than_5_days_but_more_than_1_day_before_concert(self):
-        backstage_passes = Item("Backstage passes to a TAFKAL80ETC concert", sell_in=2, quality=1)
-        gilded_rose = GildedRose([backstage_passes])
-
-        gilded_rose.update_quality()
-
-        self.assertEqual(4, backstage_passes.quality)
-
-
-    def test_backstage_passes_fall_to_zero_quality_on_the_day_of_the_concert(self):
-        backstage_passes = Item("Backstage passes to a TAFKAL80ETC concert", sell_in=0, quality=5)
-        gilded_rose = GildedRose([backstage_passes])
-
-        gilded_rose.update_quality()
-
-        self.assertEqual(0, backstage_passes.quality)
-
-    def test_backstage_passes_quality_remains_zero_after_the_concert(self):
-        backstage_passes = Item("Backstage passes to a TAFKAL80ETC concert", sell_in=-1, quality=0)
-        gilded_rose = GildedRose([backstage_passes])
-
-        gilded_rose.update_quality()
-
-        self.assertEqual(0, backstage_passes.quality)
-
-    def test_quality_of_backstage_passes_is_never_below_zero(self):
-        backstage_passes = Item("Backstage passes to a TAFKAL80ETC concert", sell_in=0, quality=0)
-        gilded_rose = GildedRose([backstage_passes])
-
-        gilded_rose.update_quality()
-
-        self.assertEqual(0, backstage_passes.quality)
-
+    @unittest.skip(reason='Test list')
+    def test_normal_item_quality_that_has_reached_its_sell_by_date_decreases_twice_as_fast(self):
+        pass
 
 if __name__ == "__main__":
     unittest.main()

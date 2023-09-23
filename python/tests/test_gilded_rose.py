6078d3d9ee4acacd6cdd9d3726bc2ed3d86799a1
hemalvarambhia <h.n.varambhia@gmail.com>
2023-09-23 11:02:43 +0100

    Added another test from the list for a concert ticket that needs to be sold between 5 and 1 day before the concert.

--- python/tests/test_gilded_rose.py
+++ python/tests/test_gilded_rose.py
@@ -37,9 +37,13 @@ class BackstagePassesTest(unittest.TestCase):
 
         self.assertEqual(15, backstage_passes.quality)
 
-    @unittest.skip(reason='Test list')
     def test_backstage_passes_increase_in_quality_by_three_less_than_5_days_but_more_than_1_day_before_concert(self):
-        pass
+        backstage_passes = Item("Backstage passes to a TAFKAL80ETC concert", sell_in=2, quality=1)
+        gilded_rose = GildedRose([backstage_passes])
+
+        gilded_rose.update_quality()
+
+        self.assertEqual(4, backstage_passes.quality)
 
     @unittest.skip(reason='Test list')
     def test_backstage_passes_fall_to_zero_quality_on_the_day_of_the_concert(self):

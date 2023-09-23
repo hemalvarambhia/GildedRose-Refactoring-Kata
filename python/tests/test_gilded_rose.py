0385bedd60dcdcea0c081cb13be687d704edc0e8
hemalvarambhia <h.n.varambhia@gmail.com>
2023-09-23 22:22:48 +0100

    Added to our test list for normal items.

--- python/tests/test_gilded_rose.py
+++ python/tests/test_gilded_rose.py
@@ -14,5 +14,10 @@ class NormalItemsTest(unittest.TestCase):
     def test_normal_item_quality_that_has_reached_its_sell_by_date_decreases_twice_as_fast(self):
         pass
 
+    @unittest.skip(reason='Test list')
+    def test_normal_item_quality_that_is_passed_its_sell_by_date_decreases_twice_as_fast(self):
+        pass
+
+
 if __name__ == "__main__":
     unittest.main()

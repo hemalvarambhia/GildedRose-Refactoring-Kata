8a04d83e595146a73a2710a7ff3d496619c3738c
hemalvarambhia <h.n.varambhia@gmail.com>
2023-09-30 20:46:12 +0100

    Added tests for sulfuras.

---
+++ python/tests/test_sulfuras.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+
+class SulfurasItemsTest(unittest.TestCase):
+
+    @unittest.skip(reason='Test list')
+    def test_sulfuras_cannot_be_sold(self):
+        pass
+
+    @unittest.skip(reason='Test list')
+    def test_sulfuras_does_not_change_in_quality(self):
+        pass

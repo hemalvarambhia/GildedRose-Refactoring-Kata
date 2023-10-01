cfcae689338416f43df341cf286bc98caa55c6d2
hemalvarambhia <h.n.varambhia@gmail.com>
2023-10-01 20:31:53 +0100

    Added a test to demonstrate that Sulfuras cannot be sold.

--- python/tests/test_sulfuras.py
+++ python/tests/test_sulfuras.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 import unittest
 
+from src.gilded_rose import GildedRose, Item
+
 
 class SulfurasItemsTest(unittest.TestCase):
 
-    @unittest.skip(reason='Test list')
+
     def test_sulfuras_cannot_be_sold(self):
-        pass
+        sulfuras = Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=23)
+        gilded_rose = GildedRose([sulfuras])
+
+        gilded_rose.update_quality()
+
+        self.assertEqual(0, sulfuras.sell_in)
 
     @unittest.skip(reason='Test list')
     def test_sulfuras_does_not_change_in_quality(self):

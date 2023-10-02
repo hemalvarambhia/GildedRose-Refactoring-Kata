19a2f7870ce2ed959f9cfcb4e614dbdd3e22181c
hemalvarambhia <h.n.varambhia@gmail.com>
2023-10-02 20:43:35 +0100

    Added an integrated/integration test involving selling multiples items and no items.

---
+++ python/tests/test_selling_multiple_items.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+
+class SellingMultipleItemsTest(unittest.TestCase):
+
+    @unittest.skip(reason='Test list')
+    def test_selling_more_than_one_item(self):
+        pass
+
+    @unittest.skip(reason='Test list')
+    def test_selling_no_items(self):
+        pass

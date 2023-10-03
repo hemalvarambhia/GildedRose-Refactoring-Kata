b0a31e81f19adf986fa572dedbe2773a172acea1
hemalvarambhia <h.n.varambhia@gmail.com>
2023-10-03 20:54:32 +0100

    A test to demonstrate how the Gilded Rose operates with multiple items.

--- python/tests/test_selling_multiple_items.py
+++ python/tests/test_selling_multiple_items.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
 import unittest
 
+from src.gilded_rose import GildedRose, Item
+
 
 class SellingMultipleItemsTest(unittest.TestCase):
 
-    @unittest.skip(reason='Test list')
     def test_selling_more_than_one_item(self):
-        pass
+        aged_brie = Item(name='Aged Brie', quality=11, sell_in=25)
+        sulfuras = Item(name='Sulfuras, Hand of Ragnaros', quality=32, sell_in=11)
+        normal_item = Item(name='Normal Item', quality=2, sell_in=5)
+        gilded_rose = GildedRose([aged_brie, sulfuras, normal_item])
+
+        gilded_rose.update_quality()
+
+        self.assertEqual(32, sulfuras.quality)
+        self.assertEqual(12, aged_brie.quality)
+        self.assertEqual(1, normal_item.quality)
 
     @unittest.skip(reason='Test list')
     def test_selling_no_items(self):

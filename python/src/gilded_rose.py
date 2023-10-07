7f341eafd0dc6e5c2df936cb7ddc0e2d7403162f
hemalvarambhia <h.n.varambhia@gmail.com>
2023-10-07 21:24:53 +0100

    Made the increment explicit in the client.

--- python/src/gilded_rose.py
+++ python/src/gilded_rose.py
@@ -12,16 +12,16 @@ class GildedRose(object):
                 case 'Sulfuras, Hand of Ragnaros':
                     pass
                 case 'Aged Brie':
-                    item.quality = self.__increase_quality_of__(item)
+                    item.quality = self.__increase_quality_of__(item, 1)
                     item.sell_in = item.sell_in - 1
                     if item.sell_in < 0:
-                        item.quality = self.__increase_quality_of__(item)
+                        item.quality = self.__increase_quality_of__(item, 1)
                 case 'Backstage passes to a TAFKAL80ETC concert':
-                    item.quality = self.__increase_quality_of__(item)
+                    item.quality = self.__increase_quality_of__(item, 1)
                     if item.sell_in < 11:
-                        item.quality = self.__increase_quality_of__(item)
+                        item.quality = self.__increase_quality_of__(item, 1)
                     if item.sell_in < 6:
-                        item.quality = self.__increase_quality_of__(item)
+                        item.quality = self.__increase_quality_of__(item, 1)
                     item.sell_in = item.sell_in - 1
                     if item.sell_in < 0:
                         item.quality = 0

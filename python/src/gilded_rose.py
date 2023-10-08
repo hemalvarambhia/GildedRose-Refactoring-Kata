a7bedf84fe45d97304932ff90db508aa4e8e761e
hemalvarambhia <h.n.varambhia@gmail.com>
2023-10-08 14:17:30 +0100

    Extracted the logic for updating backstage passes.

--- python/src/gilded_rose.py
+++ python/src/gilded_rose.py
@@ -14,19 +14,22 @@ class GildedRose(object):
                 case 'Aged Brie':
                     self.__update_quality_of_aged_brie__(item)
                 case 'Backstage passes to a TAFKAL80ETC concert':
-                    item.quality = self.__increase_quality_of__(item, 1)
-                    if item.sell_in < 11:
-                        item.quality = self.__increase_quality_of__(item, 1)
-                    if item.sell_in < 6:
-                        item.quality = self.__increase_quality_of__(item, 1)
-                    item.sell_in = item.sell_in - 1
-                    if item.sell_in < 0:
-                        item.quality = 0
+                    self.__update_quality_of_backstage_passes__(item)
                 case _:
                     item.quality = self.__reduce_quality_of(item)
                     item.sell_in = item.sell_in - 1
                     if item.sell_in < 0:
                         item.quality = self.__reduce_quality_of(item)
+
+    def __update_quality_of_backstage_passes__(self, item):
+        item.quality = self.__increase_quality_of__(item, 1)
+        if item.sell_in < 11:
+            item.quality = self.__increase_quality_of__(item, 1)
+        if item.sell_in < 6:
+            item.quality = self.__increase_quality_of__(item, 1)
+        item.sell_in = item.sell_in - 1
+        if item.sell_in < 0:
+            item.quality = 0
 
     def __update_quality_of_aged_brie__(self, item):
         item.quality = self.__increase_quality_of__(item, 1)

57d08bdc19bdd4a4be18f5017157455208a58598
hemalvarambhia <h.n.varambhia@gmail.com>
2023-11-02 18:20:22 +0000

    Refactor: made the code less imperative.

--- python/src/gilded_rose.py
+++ python/src/gilded_rose.py
@@ -49,9 +49,10 @@ class GildedRose(object):
             return -item.quality
 
     def __update_aged_brie__(self, item):
-        change = 1
         item.sell_in = item.sell_in - 1
-        if item.sell_in < 0:
+        if item.sell_in > 0:
+            change = 1
+        else:
             change = 2
 
         item.quality = min(50, max(0, item.quality + change))
